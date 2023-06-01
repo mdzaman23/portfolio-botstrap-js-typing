620364d77c23b693350ce75e550cf4ec1a3f0120
mdzaman23 <119610605+mdzaman23@users.noreply.github.com>
2023-06-01 17:04:03 +0600

    Add files via upload

---
+++ Portfolio-Bootstrap/js/script.js
@@ -0,0 +1,12 @@
+
+                        // typed
+
+                        var typed = new Typed('.typing', {
+                          strings: ['...Youtuber', 'Freelancer', 'Blogger', 'Desginer', 'Web Developer','Front-End Developer'],
+                          typeSpeed: 100,
+                          backSpeed: 60,
+                          loop: true,
+                          
+                        });
+
+  // typed
